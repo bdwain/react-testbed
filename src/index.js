5746b55e41d2cb73bf35e68018ca1d0ce1e0f2df
bwain <bwain@grubhub.com>
2016-06-16 00:31:30 -0500

    empty initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ReactTestbed extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    
+  }
+}
+
+export default ReactTestbed;
